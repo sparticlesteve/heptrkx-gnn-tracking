c850562f4ad8e149c4ba4c5d0021e5f9195c1dc3
Monir Zaman <mzaman@cray.com>
2019-10-02 16:14:34 -0500

    update xavier norm

--- models/utils.py
+++ models/utils.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 
 def init_weights(m):
     if type(m) == nn.Linear:
-        nn.init.xavier_uniform(m.weight)
+        nn.init.xavier_uniform_(m.weight)
         m.bias.data.fill_(0.01)
 
 
