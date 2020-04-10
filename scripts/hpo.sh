e30f26adf4c3f418a510f9482f2548bd73680b54
Steve Farrell <sparticlesteve@gmail.com>
2020-04-10 13:55:13 -0700

    update slurm scripts

--- scripts/hpo.sh
+++ scripts/hpo.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-#SBATCH -C gpu
 #SBATCH -J hpo
-#SBATCH --gres=gpu:8
-#SBATCH --exclusive
+#SBATCH -C gpu -N 1 -c 10
 #SBATCH --ntasks-per-node=8
-#SBATCH -c 10
+#SBATCH --gpus-per-task=1
+#SBATCH --exclusive
 #SBATCH -t 8:00:00
 #SBATCH -o logs/%x-%j.out
 
 . scripts/setup_cgpu.sh
 
-python hpo.py
+python -u hpo.py $@
--- scripts/train_cgpu.sh
+++ scripts/train_cgpu.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #SBATCH -J train-cgpu
-#SBATCH -N 1
 #SBATCH -C gpu
-#SBATCH -t 8:00:00
+#SBATCH -c 10
+#SBATCH --ntasks-per-node 8
 #SBATCH --gres=gpu:8
 #SBATCH --exclusive
+#SBATCH -t 8:00:00
 #SBATCH -d singleton
 #SBATCH -o logs/%x-%j.out
 
@@ -15,9 +16,5 @@
 mkdir -p logs
 . scripts/setup_cgpu.sh
 
-# Single GPU training
-#srun -u python train.py $@
-
 # Multi-GPU training
-srun -u -l --ntasks-per-node 8 \
-    python train.py --rank-gpu -d ddp-file $@
+srun -u -l python train.py --rank-gpu -d ddp-file $@
