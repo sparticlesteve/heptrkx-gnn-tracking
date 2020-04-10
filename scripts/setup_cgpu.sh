fdb8a1ddd304d3216459ee022f94ad874851b096
Steve Farrell <sparticlesteve@gmail.com>
2020-04-10 13:52:42 -0700

    update to pytorch 1.4.0 and Ben's latest cray-hpo

--- scripts/setup_cgpu.sh
+++ scripts/setup_cgpu.sh
@@ -1,11 +1,5 @@
 # Software environment setup for Cori GPU
-module load gcc/7.3.0
-module load cuda/10.1.168
-module load openmpi/4.0.1-ucx-1.6
-module load pytorch/v1.2.0-gpu
-
-# Libary path fix
-export LD_LIBRARY_PATH=$(dirname $(which python))/../lib/python3.6/site-packages/torch/lib:$LD_LIBRARY_PATH
+module load pytorch/v1.4.0-gpu
 
 # Setup Cray AI HPO
-export PYTHONPATH=/global/cscratch1/sd/sfarrell/cray-hpo/software
+export PYTHONPATH=/global/cscratch1/sd/bja:$PYTHONPATH
--- scripts/setup_cori.sh
+++ scripts/setup_cori.sh
@@ -1,5 +1,5 @@
 # Software environment setup for Cori Haswell and KNL
-module load pytorch/v1.2.0
+module load pytorch/v1.4.0
 
 # Setup Cray AI HPO
-export PYTHONPATH=/global/cscratch1/sd/sfarrell/cray-hpo/software
+export PYTHONPATH=/global/cscratch1/sd/bja:$PYTHONPATH
