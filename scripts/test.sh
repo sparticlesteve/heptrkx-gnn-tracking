4f829e514a553d82292ed7a6ef04f6ab0e983d14
Steve Farrell <sparticlesteve@gmail.com>
2020-04-10 14:16:58 -0700

    added basic quick test script

---
+++ scripts/test.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Quick test script to make sure things are working
+
+module purge
+module load esslurm
+. scripts/setup_cgpu.sh
+
+set -e
+
+srun -C gpu -G 8 -c 10 --ntasks-per-node 8 --exclusive -t 10 -u -l \
+    python train.py --rank-gpu -d ddp-file \
+    --n-train 32 --n-valid 32 --n-epochs 2 \
+    --output-dir $SCRATCH/heptrkx/results/test_agnn \
+    configs/agnn.yaml
+
+srun -C gpu -G 8 -c 10 --ntasks-per-node 8 --exclusive -t 10 -u -l \
+    python train.py --rank-gpu -d ddp-file \
+    --n-train 32 --n-valid 32 --n-epochs 2 \
+    --output-dir $SCRATCH/heptrkx/results/test_mpnn \
+    configs/mpnn.yaml
